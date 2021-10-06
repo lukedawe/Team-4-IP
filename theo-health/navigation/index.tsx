4d43be08886a4bce5241075b019a8950225e75c8
Calum <calumalogan@gmail.com>
2021-10-07 00:58:47 +0100

    Created base screen

--- theo-health/navigation/index.tsx
+++ theo-health/navigation/index.tsx
@@ -16,6 +16,7 @@ import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
+import SingleClientScreen from '../screens/SingleClientScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -89,6 +90,14 @@ function BottomTabNavigator() {
         component={TabTwoScreen}
         options={{
           title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="SingleClientTab"
+        component={SingleClientScreen}
+        options={{
+          title: 'Single Client Tab',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
