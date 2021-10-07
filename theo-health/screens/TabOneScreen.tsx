8ee9e7435b9d908a93a8d8e3aaa1fbc3df1e889f
AdamJohnSimpson <adamj911gmail.com>
2021-10-07 21:04:44 +0100

    added login screen components

--- theo-health/screens/TabOneScreen.tsx
+++ theo-health/screens/TabOneScreen.tsx
@@ -1,5 +1,12 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,10 +14,61 @@ import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Tab One hi</Text>
+    //   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    //   <View style={styles.container}>
+        
+    //   <Text>Open up App.js to start working on your app!</Text>
+    // </View>
+    //   {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+    // </View>
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.container2}>
+        {/* <Image style={styles.image} source={require("./assets/log2.png")} /> */}
+  
+        <StatusBar style="auto" />
+
+        <Text style={styles.heading_text}>
+          log in
+        </Text>
+
+        <Text style={styles.label_text}>
+          email
+        </Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="email"
+            placeholderTextColor="#434747"
+          />
+        </View>
+  
+        <Text style={styles.label_text}>
+          password
+        </Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="password"
+            placeholderTextColor="#434747"
+            secureTextEntry={true}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text>log in</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.info_text}>
+          don't have an account?
+        </Text>
+
+        <TouchableOpacity>
+          <Text style={styles.sign_up_button}>sign up</Text>
+        </TouchableOpacity>
+        
+      </View>
     </View>
   );
 }
@@ -18,8 +76,20 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1D2121',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container2: {
+    backgroundColor: '#434747',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingLeft: "7%",
+    paddingBottom: "5%",
+    paddingTop: "5%",
+    width: "80%",
+    //height: "50%",
+    borderRadius: 10,
   },
   title: {
     fontSize: 20,
@@ -29,5 +99,48 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  loginBtn: {
+    width: "25%",
+    borderRadius: 10,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: "#f36d21",
+  },
+  sign_up_button: {
+    height: 30,
+    justifyContent: 'flex-start',
+    color: "#f36d21",
+    //paddingLeft: 10,
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 10,
+    color: "white"
+  },
+  heading_text: {
+    color:"white",
+    fontSize: 35,
+    fontWeight: "bold",
+    paddingBottom: 10,
+  },
+  label_text: {
+    color:"#E6C59E",
+    padding: 10
+  },
+  info_text: {
+    color:"#E6C59E",
+    //marginLeft: 10,
+  },
+  inputView: {
+    backgroundColor: "#1D2121",
+    borderRadius: 10,
+    width: "90%",
+    height: 45,
   },
 });
