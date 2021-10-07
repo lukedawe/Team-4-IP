e125dc23e0d89734c6a6c0b288d5c008d7010778
Lukasz Nozewski <LNozewski@dundee.ac.uk>
2021-10-07 01:17:48 +0100

    - Added some packages - Reads json data into a simple div

--- theo-health/screens/TabOneScreen.tsx
+++ theo-health/screens/TabOneScreen.tsx
@@ -1,25 +1,47 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+
+//Creates local object "data" based on JSON file
+import * as importedData from '../example.json';
+
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+
+
+      <div className="stock-container">Hi and also Hello!</div>
+        <div className="stock-container">
+          {importedData.sessions.map((data, key) => {
+            return (
+              <div key={key}>
+                {data.id +
+                  " , " +
+                  data.date +
+                  " ," +
+                  data.comment}
+              </div>
+            );
+          })}
+        </div>
+
+
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    color:'white',
   },
   title: {
     fontSize: 20,
@@ -31,3 +53,20 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+/*
+      <Card>
+        <Card.Title>Client X</Card.Title>
+        <Card.Divider/>
+        <Card.Image source={require('../assets/images/green_background.png')}>
+          <Text style={{marginBottom: 10}}>
+            Athlete info xxxxxxxxxxxxxxxxx
+          </Text>
+          <Button
+            icon={<Icon name='code' color='#ffffff' />}
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+            title='VIEW NOW' />
+        </Card.Image>
+      </Card>
+*/
+  
