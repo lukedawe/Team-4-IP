34cfcfb751f4f8f6ff289c0c578977ea69479e80
Lukasz Nozewski <LNozewski@dundee.ac.uk>
2021-10-08 12:04:45 +0100

    -Moved session card functionality into a component

--- theo-health/screens/TabOneScreen.tsx
+++ theo-health/screens/TabOneScreen.tsx
@@ -1,61 +1,26 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { StyleSheet } from 'react-native';
+
 import EditScreenInfo from '../components/EditScreenInfo';
+import SessionCards from '../components/SessionCards';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
-
-//Creates local object "data" based on JSON file
-import * as importedData from '../example.json';
-
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={containerStyle.container}>
-      <div className="sessions-container"></div>
-          {importedData.sessions.map((data, key) => {
-            return (
-              <div key={key}>
-                <Card>
-                  <Card.Title>Session ID: {data.id}</Card.Title>
-                  <Card.Divider/>
-                  <Card.Image source={require('../assets/images/green_background.png')}>
-                    
-                    <Text style={textStyle.defaultText}>
-                      Date: {data.date}
-                    </Text>
+    <View style={styles.container}>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <SessionCards path="/screens/SessionCards.tsx" />
 
-                    <Text style={textStyle.defaultText}>
-                      Comment: {data.comment}
-                    </Text>
-
-                    <Button
-                      icon={<Icon name='code' color='#ffffff' />}
-                      buttonStyle={{marginLeft: 10, marginRight: 10, marginBottom: 10}}
-                      title='OPEN' />
-                  
-                  </Card.Image>
-                </Card>
-              </div>
-            );
-          })}
     </View>
   );
 }
 
-
-const containerStyle = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    left: '50%',
-    right: '50%',
-    top: '5%',
-    flexDirection: 'row',
-
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    color:'white',
   },
   title: {
     fontSize: 20,
@@ -66,17 +31,4 @@ const containerStyle = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-
 });
-
-
-const textStyle = StyleSheet.create({
-  defaultText:{
-    paddingLeft: 5,
-    paddingBottom: 10,
-    color: 'white',
-    fontWeight: 'normal',
-  }
-
-});
-  
