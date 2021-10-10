f2dce9aa60f47fe9cfe33faae16cf2018d9cd08a
Luke Dawe <luke.dawe@gmail.com>
2021-10-10 22:10:59 +0100

    Made an external text stylesheet

---
+++ theo-health/constants/FontStyle.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const TextStyles = StyleSheet.create({
+    baseText: {
+        fontFamily: "Cochin"
+    },
+    titleText: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+});
