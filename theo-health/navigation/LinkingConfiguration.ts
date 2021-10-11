bb0c04ba4d05919e0d291d04379cee1cce2d0a8f
Calum <calumalogan@gmail.com>
2021-10-11 06:44:11 +0100

    Fixed separation error

--- theo-health/navigation/LinkingConfiguration.ts
+++ theo-health/navigation/LinkingConfiguration.ts
@@ -25,6 +25,11 @@ const linking: LinkingOptions<RootStackParamList> = {
               TabTwoScreen: 'two',
             },
           },
+          ProgressTab: {
+            screens: {
+              ProgressScreen: 'progress',
+            },
+          },
         },
       },
       Modal: 'modal',
