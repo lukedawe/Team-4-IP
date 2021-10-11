7a659a74ae22a3cfad41c19df2ff4cf9b2b800fd
Calum <calumalogan@gmail.com>
2021-10-11 01:55:45 +0100

    Added initial formatting

--- theo-health/screens/ListViewScreen.tsx
+++ theo-health/screens/ListViewScreen.tsx
@@ -14,31 +14,38 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function ListViewScreen({ navigation }: RootTabScreenProps<'ListViewTab'>) {
+    const [firstname] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>welcome back,</Text>
-      <Text style={styles.title}>api-username</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ListViewScreen.tsx" />
-      <FlatList
-        style={{
-          flexGrow: 0,
-        }}
+    <View style={styles.mainContainer}>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/images/logo.png')}
+        />
+     </View>
+     <View style={styles.subContainer}>
+       <Text style={styles.heading_text}>
+         welcome back,
+         #api-username#
+       </Text>
+     </View>
+     <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+     <FlatList
+        style={styles.flatList}
         horizontal={true}
         data={CLIENTDATA}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <Text style={styles.listItem}>{item.name}</Text>
         )}
         keyExtractor={item => item.id}
       />
       <FlatList
-        style={{
-          flexGrow: 0,
-        }}
+        style={styles.flatList}
         horizontal={true}
         data={SESSIONDATA}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.datetime}</Text>
+          <Text style={styles.listItem}>{item.datetime}</Text>
         )}
         keyExtractor={item => item.id}
       />
@@ -47,15 +54,6 @@ export default function ListViewScreen({ navigation }: RootTabScreenProps<'ListV
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
   separator: {
     marginVertical: 30,
     height: 1,
@@ -66,15 +64,89 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
   },
-  item: {
+  listItem: {
     backgroundColor: '#ff0000',
     padding: 15,
     marginVertical: 6,
     marginHorizontal: 10,
   },
-  titletext: {
+  
+
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#1D2121',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    backgroundColor: '#1D2121',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    width: "80%",
+  },
+  subContainer: {
+    backgroundColor: '#434747',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingLeft: "7%",
+    paddingBottom: "5%",
+    paddingTop: "5%",
+    width: "80%",
+    borderRadius: 10,
+  },
+  logo: {
+    maxWidth:"100%",
+    maxHeight:"40%",
+    width: 225,
+    height: 125,
+  },
+  title: {
     fontSize: 20,
+    fontWeight: 'bold',
+  },
+  loginBtn: {
+    width: "25%",
+    borderRadius: 10,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: "#f36d21",
+  },
+  sign_up_button: {
+    height: 30,
+    justifyContent: 'flex-start',
+    color: "#f36d21",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 10,
+    color: "white"
+  },
+  heading_text: {
+    color:"white",
+    fontSize: 35,
     fontWeight: "bold",
+    paddingBottom: 10,
+  },
+  label_text: {
+    color:"#E6C59E",
+    padding: 10
+  },
+  info_text: {
+    color:"#E6C59E",
+  },
+  inputView: {
+    backgroundColor: "#1D2121",
+    borderRadius: 10,
+    width: "90%",
+    height: 45,
+  },
+  flatList: {
+    flexGrow: 0,
   },
 });
 
@@ -93,7 +165,116 @@ const CLIENTDATA = [
   {
     id: 3,
     name: 'joe bloggs',
-  }
+  },
+  {
+    id: 21,
+    name: 'jane doe',
+  },
+  {
+    id: 22,
+    name: 'john smith',
+  },
+  {
+    id: 23,
+    name: 'joe bloggs',
+  },
+  {
+    id: 31,
+    name: 'jane doe',
+  },
+  {
+    id: 32,
+    name: 'john smith',
+  },
+  {
+    id: 33,
+    name: 'joe bloggs',
+  },
+  {
+    id: 41,
+    name: 'jane doe',
+  },
+  {
+    id: 42,
+    name: 'john smith',
+  },
+  {
+    id: 43,
+    name: 'joe bloggs',
+  },
+  {
+    id: 51,
+    name: 'jane doe',
+  },
+  {
+    id: 52,
+    name: 'john smith',
+  },
+  {
+    id: 53,
+    name: 'joe bloggs',
+  },
+  {
+    id: 61,
+    name: 'jane doe',
+  },
+  {
+    id: 62,
+    name: 'john smith',
+  },
+  {
+    id: 63,
+    name: 'joe bloggs',
+  },
+  {
+    id: 71,
+    name: 'jane doe',
+  },
+  {
+    id: 72,
+    name: 'john smith',
+  },
+  {
+    id: 73,
+    name: 'joe bloggs',
+  },
+  {
+    id: 81,
+    name: 'jane doe',
+  },
+  {
+    id: 82,
+    name: 'john smith',
+  },
+  {
+    id: 83,
+    name: 'joe bloggs',
+  },
+  {
+    id: 91,
+    name: 'jane doe',
+  },
+  {
+    id: 92,
+    name: 'john smith',
+  },
+  {
+    id: 93,
+    name: 'joe bloggs',
+  },
+  {
+    id: 101,
+    name: 'jane doe',
+  },
+  {
+    id: 102,
+    name: 'john smith',
+  },
+  {
+    id: 103,
+    name: 'joe bloggs',
+  },
+
 ]
 
 const SESSIONDATA = [
