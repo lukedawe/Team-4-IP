783716072c355eadda8392f49cd0891f4b3388c5
Luke Dawe <luke.dawe@gmail.com>
2021-10-13 17:34:52 +0100

    Changed some things

--- theo-health/components/GetExerciseData.tsx
+++ theo-health/components/GetExerciseData.tsx
@@ -1,25 +1,21 @@
 import { useEffect } from 'react';
-import { ActivityIndicator, FlatList } from 'react-native';
 import { useState } from 'react';
-import { Text, View } from 'react-native';
-import React from 'react';
 
-export default function GetExerciseData() {
+export default function GetExerciseData(id: number) {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
 
     const exerciseData = async () => {
         try {
             const response = await fetch(
-                'http://localhost:5000/exercise_data/get_data', {
+                'http://localhost:5000/sessions/total_muscle_activation', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    session_id: 6,
-                    order_in_session: 1
+                    "session_id": id
                 })
             }
             )
@@ -37,21 +33,4 @@ export default function GetExerciseData() {
         exerciseData();
     }, []);
 
-    return (
-        <View style={{ flex: 1, padding: 24 }}>
-            {isLoading ? <ActivityIndicator /> : (
-                <FlatList
-                    data={data}
-                    keyExtractor={({ id }, index) => id}
-                    renderItem={({ item }) => (
-                        <Text style={{ color: "white" }}>
-                            Session ID: {item.id}{"\n"}
-                            Session date: {item.date}{"\n"}
-                            Session Comment: {item.comment}{"\n"}{"\n"}
-                        </Text>
-                    )}
-                />
-            )}
-        </View>
-    );
 };
