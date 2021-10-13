4d56386c9ba246cb52acc6c69b750246ff117d54
fincharry27 <harry.gfinch@gmail.com>
2021-10-13 10:03:21 +0100

    added heatmap integration to react project

--- theo-health/components/HeatmapComponent.tsx
+++ theo-health/components/HeatmapComponent.tsx
@@ -203,7 +203,7 @@ export default function HeatmapThing() {
                         colourChange();
                         render();
                     }
-
+                    //loop
                     GameLoop();
                 }}
             >
