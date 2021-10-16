1eefc434573e736667d2dc6dbdacb00a12fbb26b
Luke Dawe <luke.dawe@gmail.com>
2021-10-16 17:12:51 +0100

    Added more sessions to display for the bar chart

--- theo-health/screens/ProgressScreen.tsx
+++ theo-health/screens/ProgressScreen.tsx
@@ -146,7 +146,7 @@ export default function ProgressScreen({ route, navigation }: RootTabScreenProps
         {"\n"}
               Graph 1 : The line graph shows the average usage of muscles for the last 5 sessions {"\n"} {"\n"}
               Graph 2 : The doughnut chart shows the increase in usage for each muscle over your last 5 sessions {"\n"} {"\n"}
-              Graph 3 : The bar chart shows the share of work that each muscle has done for each of your last two sessions
+              Graph 3 : The bar chart shows the share of work that each muscle has done for each of your last 5 sessions
 
         </Text>
       </ScrollView>
